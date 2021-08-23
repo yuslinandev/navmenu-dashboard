63c47c21df189c2ad06d73ed1450b29f6a0029c9
Yustus <jesusvld@outlook.com>
2021-08-23 10:31:42 -0500

    fixed
    fixed js, css

--- js/script.js
+++ js/script.js
@@ -80,16 +80,19 @@ changeTheme = function(){
     var css = document.getElementById("theme");
     var imgLogo = document.getElementById("logo");
     var imgLogoFull = document.getElementById("logo-full");
+    var btnTheme = document.getElementById("btn-change-theme");
 
     fileName = css.getAttribute("href");
     if(fileName == "css/dark.css"){
         css.setAttribute("href", "css/light.css");
         imgLogo.src = "images/logo-light.png";
         imgLogoFull.src = "images/logo-full-light.png";
+        btnTheme.innerHTML = '<i class="fas fa-moon"></i>';
     }else{
         css.setAttribute("href", "css/dark.css");
         imgLogo.src = "images/logo.png";
         imgLogoFull.src = "images/logo-full.png";
+        btnTheme.innerHTML = '<i class="fas fa-sun"></i>';
     }
 }
 
